b288c812d7c759b0f2183699afc1623e6b598cf5
Bruno Santimaria <brun0.23@outlook.com>
2023-09-18 12:00:07 -0300

    modificado reducer

--- src/redux/action.js
+++ src/redux/action.js
@@ -3,11 +3,11 @@ export const GET_COMICS_BY_NAME = "GET_COMICS_BY_NAME";
 export const GET_COMIC_BY_ID = "GET_COMIC_BY_ID";
 export const GET_CHARACTERS = "GET_CHARACTERS";
 export const CLEAN_COMICS = "CLEAN_COMICS";
-
+const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
 export const getComics = () => {
   return async function (dispatch) {
     const response = await fetch(
-      "https://gateway.marvel.com:443/v1/public/comics?hasDigitalIssue=false&ts=1&format=digital%20comic&formatType=comic&noVariants=true&dateRange=2016-01-01%2C2023-01-01&orderBy=onsaleDate&limit=100&apikey=da9db239ac192993344d9b661a4cf23a&hash=2e9ddca8f096b65c3be62fc4c64a2df4"
+      `https://gateway.marvel.com:443/v1/public/comics?hasDigitalIssue=false&ts=1&format=digital%20comic&formatType=comic&noVariants=true&dateRange=2016-01-01%2C2023-01-01&orderBy=onsaleDate&limit=100&apikey=${apiKey}&hash=2e9ddca8f096b65c3be62fc4c64a2df4`
     );
     const data = await response.json();
     dispatch({ type: GET_COMICS, payload: data.data.results });
